63d07a868a5adcca3f06b58235bcbecc2f90e7d3
Clessio <clessio.silva1989@gmail.com>
2025-05-21 10:03:55 -0300

    Project created

---
+++ src/main/java/com/devsuperior/dslistmaio/DslistmaioApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistmaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistmaioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistmaioApplication.class, args);
+	}
+
+}
